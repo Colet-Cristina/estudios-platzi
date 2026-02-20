b70772c9612046c45ad1a5148e8e87d652708e12
Cristina <tu-emailejemplo.com>
2026-02-20 20:22:02 +0100

    feat: improvement

---
+++ javascript/fauna-cards/README.md
@@ -0,0 +1,18 @@
+# 🌿 Naturaleza en Código: Fauna Ibérica
+
+Este es un proyecto educativo desarrollado para practicar los fundamentos de **JavaScript**, manipulación del **DOM** y estilos dinámicos con **CSS**.
+
+## 🚀 Funcionalidades
+
+- **Generación Dinámica:** Las tarjetas de los animales se crean automáticamente a partir de un array de objetos.
+- **Formateo de Strings:** Uso de métodos como `.toUpperCase()` y `.slice()` para normalizar nombres científicos.
+- **Cálculo de Datos:** Implementación de `.reduce()` para calcular la población total protegida.
+
+## 🛠️ Tecnologías utilizadas
+
+- **HTML5:** Estructura semántica.
+- **CSS3:** Diseño responsivo con Flexbox/Grid y variables de color.
+- **JavaScript:** \* Arrays y Objetos.
+  - Métodos de array (`forEach`, `reduce`).
+  - Interpolación de strings con `` `backticks` ``.
+  - Manipulación del DOM (`querySelector`, `innerHTML`).
--- javascript/fauna-cards/js/main.js
+++ javascript/fauna-cards/js/main.js
@@ -1,74 +1,114 @@
 "use strict";
 
-// Creamos array para todos los animales
-const animals = [
-  {
-    name: `Lince Ibérico`,
-    habitat: `Doñana y Sierra Morena`,
-    status: `En peligro de extinción`,
-    family: `felino`,
-    image: `lince.png`,
-  },
-  {
-    name: `Gato Montés`,
-    habitat: `Bosques y matorrales de la Península`,
-    status: `Interés especial (amenazado por hibridación)`,
-    family: `felino`,
-    image: `gato.png`,
-  },
-  {
-    name: `Lobo Ibérico`,
-    habitat: `Sierra de la Culebra y Galicia`,
-    status: `En expansión controlada`,
-    family: `canido`,
-    image: `lobo.png`,
-  },
-  {
-    name: `Águila Imperial`,
-    habitat: `Montes de Toledo`,
-    status: `Vulnerable`,
-    family: `ave`,
-    image: `aguila.png`,
-  },
-  {
-    name: `Quebrantahuesos`,
-    habitat: `Pirineos y Picos de Europa`,
-    status: `En peligro de extinción`,
-    family: `ave`,
-    image: `gyp.png`,
-  },
+// SECCIÓN DE QUERY-SELECTOR
+// Éstos son los elementos que nos traemos de la página HTML y usamos en el código
+const app = document.querySelector(`#app`);
 
-  {
-    name: `Oso Pardo`,
-    habitat: `Cordillera Cantábrica y Pirineos`,
-    status: `En peligro de extinción`,
-    family: `ursido`,
-    image: `oso.png`,
-  },
-];
+// SECCIÓN DE DATOS
+// Variables globales que almacenan la información principal de la aplicación y se usan por todo el fichero.
 const subtitle = `Fauna Ibérica`;
 const textSubtitle = `Especies Protegidas`;
 
-// Selección del contenedor de html
-const app = document.querySelector(`#app`);
+// Creamos array para los animales
+const animals = [
+  {
+    commonName: `Lince Ibérico`,
+    scientificName: `lynx pardinus`,
+    habitat: `Doñana y Sierra Morena`,
+    status: `En peligro de extinción`,
+    family: `Felino`,
+    image: `lince.png`,
+    population: 2100,
+  },
+  {
+    commonName: `Gato Montés`,
+    scientificName: `felis silvestris`,
+    habitat: `Bosques y matorrales de la Península`,
+    status: `Interés especial (amenazado por hibridación)`,
+    family: `Felino`,
+    image: `gato.png`,
+    population: 5000,
+  },
+  {
+    commonName: `Lobo Ibérico`,
+    scientificName: `canis lupus signatus`,
+    habitat: `Sierra de la Culebra y Galicia`,
+    status: `En expansión controlada`,
+    family: `Canido`,
+    image: `lobo.png`,
+    population: 2500,
+  },
+  {
+    commonName: `Águila Imperial`,
+    scientificName: `aquila adalberti`,
+    habitat: `el centro y sur de España principalmente`,
+    status: `Vulnerable`,
+    family: `Ave`,
+    image: `aguila.png`,
+    population: 800,
+  },
+  {
+    commonName: `Quebrantahuesos`,
+    scientificName: `gypaetus barbatus`,
+    habitat: `Pirineos y Picos de Europa`,
+    status: `En peligro de extinción`,
+    family: `Ave`,
+    image: `gyp.png`,
+    population: 1000,
+  },
+  {
+    commonName: `Buitre Negro`,
+    scientificName: `aegypius monachus`,
+    habitat: `Monfragüe y Sierra de San Pedro`,
+    status: `Vulnerable`,
+    family: `Ave`,
+    image: `monachus.png`,
+    population: 2500,
+  },
+
+  {
+    commonName: `Oso Pardo`,
+    scientificName: `ursus arctos`,
+    habitat: `Cordillera Cantábrica y Pirineos`,
+    status: `En peligro de extinción`,
+    family: `Ursido`,
+    image: `oso.png`,
+    population: 400,
+  },
+];
+
+// Mapeo de colores
+const statusStyles = {
+  "En peligro de extinción": "#a52222",
+  Vulnerable: "#1d5072",
+};
 
 if (app) {
-  let htmlContent = `<h2 class="subtitle">${subtitle}</h2>`;
-  htmlContent += `<p class="text-subtitle js_textSubtitle">${textSubtitle}</p>`;
+  let htmlContent = "";
 
   // Recorremos el array para generar las tarjetas
   animals.forEach((animal) => {
     // Nombre en mayúsculas para el título
-    let displayTitle = animal.name.toUpperCase();
+    const displayTitle = animal.commonName.toUpperCase();
+
+    // .charAt(0) busca la primera letra y .toUpperCase() la convierte en mayúsculas .slice(1) para que no corte la fease
+    const scientificFormat =
+      animal.scientificName.charAt(0).toUpperCase() +
+      animal.scientificName.slice(1);
+
+    // 3. Creamos el string de estilo con interpolación.
+    const textColor = statusStyles[animal.status] || `#382920`;
+    const statusStyle = `color: ${textColor}`;
 
     htmlContent += `
       <section class="card">
         <h3 class="card-title js_cardTitle">${displayTitle}</h3>
-        <img src="./images/${animal.image}" alt="${animal.name}" class="card-img">
+        <p class="card-scientific"><i>${scientificFormat}</i></p> 
+        <img src="./images/${animal.image}" alt="${animal.commonName}" class="card-img">
         <p class="card-description js_cardDescription">
-          ${animal.name}, ${animal.family} que habita en ${animal.habitat}.
+          ${animal.family} que habita en ${animal.habitat}.
         </p>
-        <p><strong>${animal.status}</strong></p>
+        <p style="${statusStyle}"><strong>${animal.status}</strong></p>
       </section>
     `;
   });
